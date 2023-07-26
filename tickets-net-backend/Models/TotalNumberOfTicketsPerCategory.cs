cc1b9932f59b679f2ce25da1af199e7f203b99b4
Cristi Tomoiaga <Cristi-Tomoiaga@users.noreply.github.com>
2023-07-26 15:36:17 +0300

    Scaffold models and dto

---
+++ tickets-net-backend/Models/TotalNumberOfTicketsPerCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace tickets_net_backend.Models;
+
+public partial class TotalNumberOfTicketsPerCategory
+{
+    public int TicketCategoryId { get; set; }
+
+    public int? SumaBilete { get; set; }
+
+    public decimal? ValoareTotala { get; set; }
+}
